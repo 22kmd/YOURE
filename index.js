f05a8dc82378f41561b76e1c042c41536ec24df6
Andrew Dirksen <andrew@dirksen.com>
2022-12-21 16:09:58 -0800

    click as secret element to reveal all at once

--- index.js
+++ index.js
@@ -2,27 +2,25 @@ let dommes = document.getElementsByClassName("after");
 let current = 0;
 
 for (let a of dommes) {
-    a.setAttribute("hidden", true);
+  a.setAttribute("hidden", true);
 }
 
 function reveal_next() {
-    if (current < dommes.length) {
-        dommes[current].removeAttribute("hidden");
-        current += 1;
-        document.body.scrollTop = document.body.scrollHeight;
-    }
+  if (current < dommes.length) {
+    dommes[current].removeAttribute("hidden");
+    current += 1;
+
+    // scroll to bottom
+    document.body.scrollTop = document.body.scrollHeight;
+  }
 }
 
-function on_body_click() {
-    reveal_next();
-}
-
-function clicked_show_all() {
-    for (let a of dommes) {
-        a.removeAttribute("hidden");
-    }
-    this.stopPropagation();
+function clicked_show_all(event) {
+  for (let a of dommes) {
+    a.removeAttribute("hidden");
+  }
+  event.stopImmediatePropagation();
 }
 
 document.body.onkeydown = reveal_next;
-document.body.onclick = on_body_click;
+document.body.onclick = reveal_next;
